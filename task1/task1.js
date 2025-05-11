294ccfde7335f70d210c9249c7b012b2b93a1941
Daniel <dymx103@gmail.com>
2025-05-11 13:20:37 +0800

    completed task 1

---
+++ task1/task1.js
@@ -0,0 +1,16 @@
+// 1. Variable Declaration and Data Types (5 Marks) 
+
+// a) Declare variables to store your name (string), age (number), and isStudent (boolean). 
+
+// b) Assign appropriate values to each variable, for example: name = "Alice", age = 22, isStudent = true. 
+
+// c) Use console.log() to display each variable along with its data type. 
+
+
+let name, age, isStudent;
+name = "Alice";
+age = 22;
+isStudent = true;
+console.log("name - value: " + name + ", type: " + typeof (name));
+console.log("age - value: " + age + ", type: " + typeof (age));
+console.log("isStudent - value: " + isStudent + ", type: " + typeof (isStudent));
